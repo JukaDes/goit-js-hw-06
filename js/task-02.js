3f99ff293ac67d195c1cf3e6484f4c8719b02f62
Jekaterina Sedjuka <jekaterina.sedjuka@gmail.com>
2022-09-28 08:25:15 +0100

    Completed task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+let el = [];
+
+ingredients.forEach((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  li.classList.add("item");
+  el.push(li);
+});
+
+list.append(...el);
+
+console.log(list);
