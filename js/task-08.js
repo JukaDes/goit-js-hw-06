6782b3bbdfac7279d3462e8673a7d42e521a9f14
Jekaterina Sedjuka <jekaterina.sedjuka@gmail.com>
2022-09-29 07:15:43 +0100

    Completed task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector('.login-form');
+const emailElement = form.firstElementChild;
+const passwordElement = emailElement.nextElementSibling;
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please complete form!!!');
+  }
+
+  const userData = {};
+  userData[emailElement.outerText] = email.value;
+  userData[passwordElement.outerText] = password.value;
+  console.log(userData);
+
+  event.currentTarget.reset();
+});
